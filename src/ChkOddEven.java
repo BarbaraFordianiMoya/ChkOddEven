05cbe804accb3761fb55e20ec99b7c9e11e25d32
Aluno <Aluno@10.85.198.10>
2024-06-05 16:20:28 -0300

    ChkOddEven

---
+++ src/ChkOddEven.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class ChkOddEven {
+
+    public static void main(String args[]) {
+
+        int nb = Integer.parseInt(JOptionPane.showInputDialog(null,"Escolha um numero de 1 a 10:"));
+        if (nb == 1) {
+        System.out.println("O numero 1 e impar");
+    }
+        if (nb == 2) {
+        System.out.println("O numero 2 e par");
+    }
+        if (nb == 3) {
+        System.out.println("O numero 3 e impar");
+    }
+        if (nb == 4) {
+        System.out.println("O numero 4 e par");
+    }
+        if (nb == 5) {
+        System.out.println("O numero 5 e impar");
+    }
+        if (nb == 6) {
+        System.out.println("O numero 6 e par");
+    }
+        if (nb == 7) {
+        System.out.println("O numero 7 e impar");
+    }
+        if (nb == 8) {
+        System.out.println("O numero 8 e par");
+    }
+        if (nb == 9) {
+        System.out.println("O numero 9 e impar");
+    }
+        if (nb == 10) {
+        System.out.println("O numero 10 e par");
+    }
+    }
+}
